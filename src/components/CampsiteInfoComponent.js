bfbd880e59ec7116906a61ccd4aaa6b30ea3a4a3
Brandon Hamburg <brando.hamburg@gmail.com>
2021-11-16 21:38:31 -0500

    Deleted unused code and comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,13 +1,5 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
-//import { COMMENTS } from "../shared/comments";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class CampsiteInfoComponent extends Component {
   renderCampsite(campsite) {
